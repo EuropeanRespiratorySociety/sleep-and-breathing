7ea06ce4c1cebd7ef1736917c5a6e8dc5611d8bd
tasanoers <tamaki.asano@ersnet.org>
2019-03-22 08:50:56 +0100

    Remove the pdf's update info

--- src/components/widgets/AccessProgramme.vue
+++ src/components/widgets/AccessProgramme.vue
@@ -1,32 +1,45 @@
 <template>
-  <v-card id="test" >
+  <v-card id="test">
     <v-card-title primary-title>
       <div>
         <h3 class="headline mb-0">Access Programme 2019</h3>
       </div>
     </v-card-title>
     <v-card-text>
-      Please use one of the button below to access our programme.<br /><spam class="caption">&nbsp;(PDF document : Last updated on <strong>12 November</strong>)</spam>
+      Please use one of the button below to access our programme.
+      <!-- <br>
+      <span class="caption">
+        &nbsp;(PDF document : Last updated on
+        <strong>12 November</strong>)
+      </span>-->
     </v-card-text>
     <div class="ml-2 pb-3">
-      <a target="_blank" style="text-decoration: none;" href="http://k4.ersnet.org/prod/v2/front/program/?e=114">
+      <a
+        target="_blank"
+        style="text-decoration: none;"
+        href="http://k4.ersnet.org/prod/v2/front/program/?e=114"
+      >
         <v-btn color="info" class="ml-2 mb-2">Browse the online programme</v-btn>
       </a>
-      <a target="_blank" style="text-decoration: none;" href="https://ers.box.com/shared/static/h73cc6rm9pdc5j4hao8e7qgk0506jz2a.pdf">
+      <a
+        target="_blank"
+        style="text-decoration: none;"
+        href="https://ers.box.com/shared/static/h73cc6rm9pdc5j4hao8e7qgk0506jz2a.pdf"
+      >
         <v-btn flat color="info" class="ml-2 mb-2">
           <v-icon left dark>save_alt</v-icon>Download the programme in PDF
         </v-btn>
       </a>
-      <br />
+      <br>
     </div>
   </v-card>
 </template>
 <script>
 export default {
-  name: 'access-programme'
-}
+  name: "access-programme"
+};
 </script>
 
 <style>
-  @import '../../css/fonts.css';
+@import "../../css/fonts.css";
 </style>
