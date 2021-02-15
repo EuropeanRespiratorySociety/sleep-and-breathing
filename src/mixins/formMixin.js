89a303160d666755b13beb360f289ba6be26b00b
tasanoers <tamaki.asano@ersnet.org>
2021-02-15 16:27:24 +0100

    Get back the first video to the homepage

--- src/mixins/formMixin.js
+++ src/mixins/formMixin.js
@@ -24,17 +24,17 @@ export const formMixin = {
       return {
         videoNews: [
           {
-            videoNewsUrl: 'https://vimeo.com/510715784',
-            videoNewsTitle: 'Cough assistance and secretion management in neuromuscular disease',
-            videoNewsSubTitle: '',
-            videoNewsLead: 'Tiina Andersen (Bergen, Norway). Cough assistance and secretion management in neuromuscular disease.'
-          },
-          {
             videoNewsUrl: 'https://www.youtube-nocookie.com/embed/zJG1toNLn5U?rel=0',
             videoNewsTitle: 'Sleep and Breathing 2021',
             videoNewsSubTitle: '',
             videoNewsLead: 'Taking place online from 16–17 April, 2021, the Sleep and Breathing conference offers an integrated approach to the investigation and treatment of sleep disorders and will focus on professional development and education. The conference is organised by the European Respiratory Society (ERS) and the European Sleep Research Society (ESRS).'
-          }
+          },
+          // {
+          //   videoNewsUrl: 'https://vimeo.com/510715784',
+          //   videoNewsTitle: 'Cough assistance and secretion management in neuromuscular disease',
+          //   videoNewsSubTitle: '',
+          //   videoNewsLead: 'Tiina Andersen (Bergen, Norway). Cough assistance and secretion management in neuromuscular disease.'
+          // },
         //   {
         //     videoNewsUrl: 'https://www.youtube.com/embed/ddSHD-arwC4',
         //     videoNewsTitle: 'Air pollution and COVID-19 - Clearing the air and charting a post-pandemic course',
